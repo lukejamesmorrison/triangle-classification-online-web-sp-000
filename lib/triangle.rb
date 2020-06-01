1be3b0ae5c00ab9812cfac39c6f54c790c9b443c
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 19:41:03 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+
+    if !valid? || illegal?
+        raise TriangleError
+    else
+      # equilateral
+      if @side_a == @side_b && @side_a == @side_c
+        :equilateral
+      # isosceles
+      elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+        :isosceles
+      #scalene
+      else
+        :scalene
+      end
+    end
+  end
+
+  def illegal?
+    @side_a == 0 && @side_b == 0 && @side_c == 0
+  end
+
+  def valid?
+    @side_a + @side_b > @side_c && @side_a + @side_c > @side_b && @side_b + @side_c > @side_a
+  end
+
 end
