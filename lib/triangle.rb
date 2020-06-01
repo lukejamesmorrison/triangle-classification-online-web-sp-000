06b70717e95236cf8a75cecddf56151714572677
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 19:36:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
       if @side_a == @side_b && @side_a == @side_c
         :equilateral
       # isoceles
-      elsif @side_a == @side_b || @side_a == @side_c
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
         :isoceles
       #scalene
       else
